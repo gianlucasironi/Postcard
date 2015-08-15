46c15a7fa9cf5afaf6b95e7482e4f5ee6b6f7eac
sironi-gl <sironi.gl@gmail.com>
2015-08-15 19:22:15 +0200

    added a Label for the Name

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var enterNameTextFIeld: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
     
@@ -32,6 +35,13 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
